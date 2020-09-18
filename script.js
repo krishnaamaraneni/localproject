c026e1bd48c84ac7a5b899b9cb2fe9fb4c935a63
krishnaamaraneni <71481794+krishnaamaraneni@users.noreply.github.com>
2020-09-18 15:10:40 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+//#1 document.write
+	//document.write( 12344*123)
+
+//#2 console.log()
+	//console.log("Hi there %cWeirdo" ,"color:white; background:black")
+	//console.log(100 * 4 - 250)
+
+
+//#3 window.alert() / alert()
+	//alert(100 * 4 - 250)
+
+//#4 Writing into HTML elements
+	var addHeading = document.getElementById("heading2")
+	addHeading.innerHTML = "I have been added "
+
+	
+
+	
